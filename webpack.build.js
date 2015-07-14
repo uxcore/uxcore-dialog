c23de78e3eea7e84410f8bf9f24797c801e9a796
macisi <macisi528@gmail.com>
2015-07-14 11:45:30 +0800

    up

--- webpack.build.js
+++ webpack.build.js
@@ -4,6 +4,7 @@
  */
 var webpack = require('webpack');
 var loaders = require('./loader.config');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -12,11 +13,17 @@ module.exports = {
         standalone: './example/standalone.jsx'
     },
     output: {
-        path: './build',
+		path: './build',
         publicPath: '/',
         filename: '[name].bundle.js',
         chunkFilename: '[name].[id].bundle.js'
     },
+	plugins: ['index', 'inline', 'simple', 'standalone'].map(function(name){
+			return new HtmlWebpackPlugin({
+				filename: name + '.html',
+	      		template: './example/' + name + '.html'
+			});
+		}),
     module: {
         loaders: loaders
     },
